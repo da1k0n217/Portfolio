b95aefc58b0c7872baf6cb15783b2356004bae75
DAI-KON <d2a7d2al7ps@gmail.com>
2025-10-23 23:24:11 +0900

    headerいじった

---
+++ src/lib/header.svelte
@@ -0,0 +1,19 @@
+<script>
+
+
+
+</script>
+
+<header>
+
+    <div class="w-full h-18 border-b flex justify-center items-center">
+
+        <div class="">
+
+            <h1 class="text-2xl font-light p-4">DK</h1>
+
+        </div>
+
+    </div>
+
+</header>
